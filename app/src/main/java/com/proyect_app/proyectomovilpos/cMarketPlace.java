5256be84d1048801bb92cd25c1bd0f0306ab348b
inwaytosenior <papatu10u18@gmail.com>
2024-03-29 13:12:33 -0600

    cada apartado ya tiene su boton de volver

--- app/src/main/java/com/proyect_app/proyectomovilpos/cMarketPlace.java
+++ app/src/main/java/com/proyect_app/proyectomovilpos/cMarketPlace.java
@@ -1,6 +1,9 @@
 package com.proyect_app.proyectomovilpos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,22 @@ public class cMarketPlace extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnBack01 = (Button) findViewById(R.id.btnBack01);
+
+        btnBack01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(cMarketPlace.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+
+
+
     }
 }
